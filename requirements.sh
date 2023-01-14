89acf3c53a97c599844ad74c064d174217ead6bc
Pablo Gonzalez <pablo.pabliskidx9@gmail.com>
2023-01-14 02:22:08 -0600

    Project packages added in requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-sudo su
-
 apt update
+apt upgrade -y
 apt install software-properties-common -y
 
 add-apt-repository ppa:alexlarsson/flatpak
 
-apt upgrade -y
+apt update
 apt install xserver-xorg x11-xserver-utils xinit -y
-apt install openbox lxterminal -y
-apt install pulseaudio -y
+apt install openbox -y
+apt install pulseaudio minizip -y
 
 apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 wget https://flathub.org/repo/appstream/com.snes9x.Snes9x.flatpakref
-flatpak install com.snes9x.Snes9x.flatpakref
+flatpak install com.snes9x.Snes9x.flatpakref -y
 
 apt install antimicro -y
+
+apt install feh -y
+
+apt install git debhelper build-essential
+
+mkdir /home/pi/ROMS
 
 reboot
 
