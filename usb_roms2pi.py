4dabf0c2b2fed2d6999e9c874bc2011d042da98d
P-DX-U <81974601+P-DX-U@users.noreply.github.com>
2023-01-14 20:00:39 -0600

    Update usb_roms2pi.py
    Comments and led indicator of usb unmounted added.

--- usb_roms2pi.py
+++ usb_roms2pi.py
@@ -13,37 +13,61 @@
 #
 # ## ############################################################
 
-import os
-import shutil
-import time
+import os # Import miscellaneous operating system interfaces.
+import shutil # Import High-level file operation to copy the roms.
+from time import sleep # Import pause function.
+import RPi.GPIO as GPIO # Imports GPIO control of Raspberry Pi.
 
-rutaOrigen = '/media/usb0/ROMS/'
-rutaDestino = '/home/pi/ROMS/'
+# Mounting point of the usb storage drive with ROMS folder inside (Source).
+srcPath = '/media/usb0/ROMS/'
+# ROMS folder in the Raspberry Pi (Destination).
+destPath = '/home/pi/ROMS/'  
 
+GPIO.setwarnings(False) # Disable warnings.
+GPIO.setmode(GPIO.BOARD) # Library config to use an especific pin.
+GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW) # Pin 32 as output and enable in low. 
+
+# Function to copy rom files from usb drive into the Raspberry Pi ROMS path.
 def romsCopy():
-    while True:
-        try:
-            if os.path.exists(rutaOrigen) == True:
-                if len(os.listdir(rutaOrigen)) >= 1:
-                    archivos = os.listdir(rutaOrigen)
-                    print(archivos)
+    while True: # Program works every time
+        try: # Exception handling.
+            # Detects if the usb drive is plugged in.
+            if os.path.exists(srcPath) == True:
+                # Starts if there is one or more files.
+                if len(os.listdir(srcPath)) >= 1:
+                    # Creates a list with the name of the files.
+                    archivos = os.listdir(srcPath)
+                    # print(archivos)
                     for i in archivos:
-                        if os.path.exists(rutaDestino + i) == False:
+                        # If the rom does not exists in the destination path,
+                        # it is copied.
+                        if os.path.exists(destPath + i) == False:
                             if i.endswith(".smc"):
-                                shutil.copy(rutaOrigen + i, rutaDestino)
+                                shutil.copy(srcPath + i, destPath)
                             if i.endswith(".sfc"):
-                                shutil.copy(rutaOrigen + i, rutaDestino)
+                                shutil.copy(srcPath + i, destPath)
                             if i.endswith(".zip"):
-                                shutil.copy(rutaOrigen + i, rutaDestino)
-                        else:
-                            print('Archivo ' + i + ' repetido.\n')
+                                shutil.copy(srcPath + i, destPath)
+                        else: # Skips the repeated file.
+                            # print('The file > ' + i + ', is repeated.\n')
                             pass
+                    # Dismount the usb drive when checks every file or the path
+                    # is empty.
                     os.system('sudo umount /media/usb0')
-                    print('USB desmontado.\n')
+                    #print('USB desmontado.\n')
+                    # The led lights up to indicate that the usb drive 
+                    # can be disconnected.
+                    GPIO.output(32, GPIO.HIGH)
+                    sleep(3)
                 else:
                     os.system('sudo umount /media/usb0')
-                    print('USB desmontado.\n')
-            else:
+                    #print('USB desmontado.\n')
+                    GPIO.output(32, GPIO.HIGH)
+                    sleep(3)
+
+            else: # Take a pause when the usb drive is unmounted.
+                # Turns off the led and restarts the sequence.
+                GPIO.output(32, GPIO.LOW) 
                 time.sleep(1)
 
         except KeyboardInterrupt:
